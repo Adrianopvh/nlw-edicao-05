23ff98c21461aea4e0fe7626ef4a0c1ff6aedb6c
Adrianopvh <adriano.rodriques12389@gmail.com>
2021-04-25 16:32:25 -0400

    Finalizando NLW05 aula-05

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -1,6 +1,6 @@
 import { getCustomRepository, Repository } from "typeorm";
 import { Connection } from "../entities/Connection";
-import {ConnectionsRepository  } from "../repositories/ConnectionsRepository";
+import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
 
 interface IConnectionCreate {
     socket_id: string;
@@ -30,9 +30,37 @@ class ConnectionsService {
     }
 
     async findByUserId(user_id: string) {
-        const connection = await this.connectionsRepository.findOne({user_id});
+        const connection = await this.connectionsRepository.findOne({ user_id });
 
         return connection;
+    }
+
+    async findAllWithoutAdmin() {
+        const connections = await this.connectionsRepository.find({
+            where: { admin_id: null },
+            relations: ["user"]
+        });
+
+        return connections;
+    }
+
+    async findBySocketID(socket_id: string) {
+        const connecion = await this.connectionsRepository.findOne({
+            socket_id
+        });
+
+        return connecion;
+    }
+
+    async updateAdminID(user_id: string, admin_id: string) {
+        await this.connectionsRepository
+            .createQueryBuilder()
+            .update(Connection)
+            .set({ admin_id })
+            .where('user_id = :user_id', {
+                user_id,
+            })
+            .execute();
     }
 }
 
